bd0756a95103814dac71aba5f20b4228ce99fe58
Corentin <36472830+Corentints@users.noreply.github.com>
2024-05-06 23:41:17 +0000

    Sync LeetCode submission Runtime - 68 ms (33.58%), Memory - 52.7 MB (42.80%)

---
+++ src/0206-reverse-linked-list/solution.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function dst(item: ListNode | null, head: ListNode | null) {
+    if (item === null) {
+        return head;
+    }
+
+    return dst(item.next, new ListNode(item.val, head));
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+    if (head === null) {
+        return null;
+    }
+
+     return dst(head.next, new ListNode(head.val, null));
+};
